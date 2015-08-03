7e2324c211c367b03581dc7fde8eeaf5e7fc142b
iaolier <iaolier@gmail.com>
2015-08-04 00:36:41 +0100

    more doc

--- R/class-tree.R
+++ R/class-tree.R
@@ -9,6 +9,17 @@
   )
 )
 
+#' @title Convert a data frame to a tree object
+#'
+#' @description
+#' \code{as.tree} returns a tree object
+#'
+#' @param df A data frame.
+#' @param id.col Column name which contains the node IDs (default: "id").
+#' @param parent.id.col Column name which contains the parent node IDs (default: "parent.id").
+#' @param name.col Column name which contains the node names (default: "name").
+#' @return \code{as.tree} returns a tree object.
+#'
 as.tree <- function(df, id.col = "id", parent.id.col = "parent.id", name.col = "name"){
   tmp <- lapply(1:nrow(df), function(x){
     .node(id = as.character(df[x, id.col]), parent.id = as.character(df[x, parent.id.col]), name = as.character(df[x, name.col]))
@@ -17,10 +28,20 @@ as.tree <- function(df, id.col = "id", parent.id.col = "parent.id", name.col = "
   .tree(tree.list = tmp)
 }
 
+
 setGeneric(name="distToRoot",
            def=function(object, node.id){standardGeneric("distToRoot")}
 )
 
+#' @title Compute distance to the Root node
+#'
+#' @description
+#' \code{distToRoot} returns the distance from a particular node to the root node.
+#'
+#' @param object A tree object.
+#' @param node.id The ID of the node.
+#' @return \code{distToRoot} returns a numeric value.
+#'
 setMethod(
   f="distToRoot", signature="tree",
   definition=function(object, node.id){
@@ -37,6 +58,16 @@ setGeneric(name="distNodes",
            def=function(object, node1.id, node2.id){standardGeneric("distNodes")}
 )
 
+#' @title Compute distance between two nodes.
+#'
+#' @description
+#' \code{distNodes} returns the distance between two nodes.
+#'
+#' @param object A tree object.
+#' @param node1.id The ID of the 1st node.
+#' @param node2.id The ID of the 2nd node.
+#' @return \code{distNodes} returns a numeric value.
+#'
 setMethod(
   f="distNodes", signature="tree",
   definition=function(object, node1.id, node2.id){
@@ -49,6 +80,15 @@ setGeneric(name="getPathToRoot",
            def=function(object, node.id){standardGeneric("getPathToRoot")}
 )
 
+#' @title Get path to root.
+#'
+#' @description
+#' \code{getPathToRoot} returns the path from a particular node to root.
+#'
+#' @param object A tree object.
+#' @param node.id The ID of the 1st node.
+#' @return \code{getPathToRoot} returns a character vector.
+#'
 setMethod(
   f="getPathToRoot", signature="tree",
   definition=function(object, node.id){
@@ -65,6 +105,16 @@ setGeneric(name="lca",
            def=function(object, node1.id, node2.id){standardGeneric("lca")}
 )
 
+#' @title Find the lowest common ancestor.
+#'
+#' @description
+#' \code{lca} returns the ID of the lowest common ancestor between two nodes.
+#'
+#' @param object A tree object.
+#' @param node1.id The ID of the 1st node.
+#' @param node2.id The ID of the 2nd node.
+#' @return \code{lca} returns a character value.
+#'
 setMethod(
   f="lca", signature="tree",
   definition=function(object, node1.id, node2.id){
@@ -81,6 +131,14 @@ setGeneric(name="distMatrix",
            def=function(object){standardGeneric("distMatrix")}
 )
 
+#' @title Compute a matrix of distances between all nodes.
+#'
+#' @description
+#' \code{distMatrix} returns a matrix of distances between all nodes.
+#'
+#' @param object A tree object.
+#' @return \code{distMatrix} returns a numeric value.
+#'
 setMethod(
   f="distMatrix", signature="tree",
   definition=function(object){
